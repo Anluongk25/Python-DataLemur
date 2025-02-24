0877cdf31ff6dd0b726f37c3a08d636eb2e97c9a
Anluongk25 <luongmyan1502@gmail.com>
2025-02-24 21:10:22 +0700

    same_stripes

---
+++ same_stripes.py
@@ -0,0 +1,14 @@
+def is_same_stripes(matrix):
+	
+   
+    diagonal_values = {}
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            key = i - j  # Chỉ số đại diện cho một đường chéo duy nhất
+            if key not in diagonal_values:
+                diagonal_values[key] = matrix[i][j]
+            elif diagonal_values[key] != matrix[i][j]:
+                return False  # Nếu có sự khác biệt, trả về False ngay lập tức
+
+    return True  # Tất cả các đường chéo hợp lệ
